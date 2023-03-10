30ef2b226120e6580a9df2446db42787e652d79a
Humaira Fasih <humairafasih18@gmail.com>
2023-03-10 23:51:49 +0500

    set up frontend config and structure

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function App() {
+  const dummyStyle = {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  };
+
+  return (
+    <div className="App" style={dummyStyle}>
+      <header className="App-header">
+        <h1> All set up! </h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
