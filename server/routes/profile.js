c56517f9ad3bc69b7cea87cc64e5523046aa724d
MuhammadSaad0 <muhammadsaadz100@gmail.com>
2023-03-24 16:37:20 +0500

    Update Counselor Backend Set/Update Availability-Saad

--- server/routes/profile.js
+++ server/routes/profile.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const jwtDecode = require('jwt-decode');
 const Student = require('../models/studentModel');
+const Counselor = require('../models/counselorModel');
+const Availability = require('../models/availabilityModel');
 
 // create a router
 const router = express.Router();
@@ -14,6 +16,7 @@ router.post('/updateprofile', async (req, res) => {
   // get id and usertype from the decoded token
   const { id } = jwtDecoded;
   const { usertype } = jwtDecoded;
+  const { username } = jwtDecoded;
 
   // if student, then update student profile, else update counselor profile
   if (usertype === 'Student') {
@@ -28,6 +31,46 @@ router.post('/updateprofile', async (req, res) => {
           last_name: newlastname,
           dob: newdob,
           gender: newgender,
+        }
+      );
+      res.send('Success');
+    } catch (error) {
+      res.send(error);
+    }
+  } else if (usertype === 'Counselor') {
+    try {
+      // get counselor data and update the record
+      const {
+        newfirstname,
+        newlastname,
+        newdob,
+        newexperience,
+        newqualification,
+        newbio,
+        newgender,
+        newdaytype,
+        newtime,
+        newmeridian,
+      } = req.body;
+      await Counselor.update(
+        { _id: id },
+        {
+          first_name: newfirstname,
+          last_name: newlastname,
+          date_of_birth: newdob,
+          gender: newgender,
+          bio: newbio,
+          qualification: newqualification,
+          experience: newexperience,
+        }
+      );
+      // update counselor availibility
+      const newslot = newtime + newmeridian;
+      await Availability.update(
+        { counselor_username: username },
+        {
+          day_type: newdaytype,
+          time: newslot,
         }
       );
       res.send('Success');
