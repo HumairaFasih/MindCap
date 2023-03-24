29c8ef165d73546557d6550911a381af3b6eacc8
sumairijazhashmi <sumairijazhashmi@gmail.com>
2023-03-24 19:25:07 +0500

    view reviews and rating ka backend

--- server/routes/rate.js
+++ server/routes/rate.js
@@ -31,4 +31,35 @@ router.post('/addreview', async (req, res) => {
   }
 });
 
+router.get('/getreviews', async (req, res) => {
+  // access and decode token
+  const token = req.cookies.jwt;
+  console.log(token);
+  const jwtDecoded = jwtDecode(token);
+
+  // get id and usertype from the decoded token
+  // const { id } = jwtDecoded;
+  const { usertype } = jwtDecoded;
+  const { username } = jwtDecoded;
+  
+  let counselorUsername;
+  // if student or admin is accessing reviews, then counselor's username is in the request, otherwise the username is in the token.
+  if(usertype === 'Student' || usertype === 'Admin') {
+    counselorUsername = req.body.counselorusername;
+  }
+  else {
+    counselorUsername = username;
+  }
+  try {
+    const reviews = await Reviews.getdetails({
+      counselor_username: counselorUsername,
+    })
+    res.send([...reviews]);
+  }
+  catch (err) {
+    res.send(err);
+  }
+
+});
+
 module.exports = router;
