94ea096a58a716c9db58b3171d4fbd2ff14e7890
Humaira Fasih <humairafasih18@gmail.com>
2023-04-04 20:52:35 +0500

    fixed book appointment route

--- client/src/components/BookAppointmentCard.jsx
+++ client/src/components/BookAppointmentCard.jsx
@@ -1,15 +1,57 @@
-import * as React from 'react';
-import {Box, Typography, Button} from '@mui/material';
+import React from 'react';
+import { Box, Typography, Button } from '@mui/material';
+import { useNavigate, useLocation } from 'react-router-dom';
 
 function BookAppointmentCard() {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const bookAppointmentRoute = `${location.pathname
+    .split('/')
+    .slice(0, -1)
+    .join('/')}/book-appointment`;
+
   return (
     <Box>
-        <Box sx={{display:'flex', flexDirection:'row'}}>
-        <Box sx={{display: 'flex', flexDirection:'column', justifyContent:'center', alignItems:'center', width: '250px', height: '180px', bgcolor: '#D6E0CE', borderRadius: '10px', p:'15px', m:'15px', border: '2px solid #B0C9A1'}}>
-        <Button sx={{display:'flex', alignItems:'center', justifyContent:'center', borderRadius:'50%', bgcolor:'#93B77D', width: '70px', height:'70px', color: 'white', fontSize: '40px', fontWeight:'bold', textAlign: 'center'}}>+</Button>
-        <Typography sx={{fontSize:'20px', fontWeight:'bold', p:'10px'}}>Book Appointment</Typography>
+      <Box sx={{ display: 'flex', flexDirection: 'row' }}>
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+            width: '250px',
+            height: '180px',
+            bgcolor: '#D6E0CE',
+            borderRadius: '10px',
+            p: '15px',
+            m: '15px',
+            border: '2px solid #B0C9A1',
+          }}
+        >
+          <Button
+            sx={{
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+              borderRadius: '50%',
+              bgcolor: '#93B77D',
+              width: '70px',
+              height: '70px',
+              color: 'white',
+              fontSize: '40px',
+              fontWeight: 'bold',
+              textAlign: 'center',
+            }}
+            onClick={() => navigate(bookAppointmentRoute)}
+          >
+            +
+          </Button>
+          <Typography sx={{ fontSize: '20px', fontWeight: 'bold', p: '10px' }}>
+            Book Appointment
+          </Typography>
         </Box>
-        </Box>
+      </Box>
     </Box>
   );
 }
