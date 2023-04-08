32ad0cbfdd028e9de7ba31f46cdeb8fe068e4f9d
Humaira Fasih <humairafasih18@gmail.com>
2023-04-08 19:23:47 +0500

    fix login error; add update-password backend

--- server/routes/authenticate.js
+++ server/routes/authenticate.js
@@ -1,6 +1,8 @@
 // Router that handles requests sent to the /api/authenticate route
 const express = require('express');
 const jwt = require('jsonwebtoken');
+const jwtDecode = require('jwt-decode');
+const bcrypt = require('bcrypt');
 const Admin = require('../models/adminModel');
 const Counselor = require('../models/counselorModel');
 const Student = require('../models/studentModel');
@@ -19,7 +21,7 @@ const createToken = (id, username, usertype) =>
   });
 
 // find and match credentials, if successful set jwt cookie and respond back with object id
-router.post('/login', async (req, res) => {
+router.post('/login', async (req, res, next) => {
   const { username, password } = req.body;
 
   // try catch hell since we wont have jwt token and thus no usertype to make search easier
@@ -27,9 +29,6 @@ router.post('/login', async (req, res) => {
     const student = await Student.login(username, password);
     console.log(student);
     const jwtToken = createToken(student.id, student.username, 'Student');
-    // console.log(jwtToken);
-    // const jwtDecoded = jwtDecode(jwtToken);
-    // console.log(jwtDecoded);
     res.cookie('jwt', jwtToken, { maxAge: expireTime * 1000 });
     res.json({ id: student.id });
   } catch (_) {
@@ -49,8 +48,36 @@ router.post('/login', async (req, res) => {
         res.cookie('jwt', jwtToken, { maxAge: expireTime * 1000 });
         res.json({ id: admin.id });
       } catch (err) {
-        console.log(err);
+        res.status(500).json({ message: err.message });
+        next(err);
       }
+    }
+  }
+});
+
+router.patch('/update-password', async (req, res, next) => {
+  const token = req.cookies.jwt;
+  const jwtDecoded = jwtDecode(token);
+  const { id, usertype } = jwtDecoded;
+  const { newPassword } = req.body;
+  const salt = await bcrypt.genSalt();
+  const hashed = await bcrypt.hash(newPassword, salt);
+
+  if (usertype === 'Student') {
+    try {
+      await Student.update({ _id: id }, { password: hashed });
+      res.send('Password updated!');
+    } catch (err) {
+      res.status(500).json({ message: err.message });
+      next(err);
+    }
+  } else if (usertype === 'Counselor') {
+    try {
+      await Counselor.update({ _id: id }, { password: hashed });
+      res.send('Password Updated');
+    } catch (err) {
+      res.status(500).json({ message: err.message });
+      next(err);
     }
   }
 });
