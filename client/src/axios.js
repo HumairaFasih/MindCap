e68426ae594cf158cfa16db41e6424b034d41d75
Humaira Fasih <humairafasih18@gmail.com>
2023-04-08 19:20:27 +0500

    add axios interceptor

---
+++ client/src/axios.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+export const instance = axios.create({
+  baseURL: 'http://localhost:3003/api',
+  withCredentials: true,
+  headers: { 'Content-Type': 'application/json' },
+});
+
+instance.interceptors.request.use(
+  (config) => {
+    // Get the token
+    const token = localStorage.getItem('token');
+
+    // Add the token to the request headers
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
