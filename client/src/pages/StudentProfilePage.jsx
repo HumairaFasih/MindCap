29b90ee06c8a1fce59ddfb6e60a9c1ebfa2798c7
Ahmed Iqbal <88142581+ahmediq-git@users.noreply.github.com>
2023-04-14 16:10:13 +0500

    Fixed Student Profile Page responsiveness

--- client/src/pages/StudentProfilePage.jsx
+++ client/src/pages/StudentProfilePage.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useContext } from 'react';
+import React, { useState, useEffect, useContext, useCallback } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
 import axios from 'axios';
 import { Box, Typography, Divider, Icon } from '@mui/material';
@@ -29,6 +29,7 @@ function StudentProfile() {
 
   const [studentDetails, setStudentDetails] = useState('');
   const [loaded, setLoaded] = useState(false);
+  const [screenSize, setScreenSize] = useState('');
 
   const viewMedicalRecord = async (e) => {
     e.preventDefault();
@@ -55,6 +56,76 @@ function StudentProfile() {
         console.log(err);
       });
   }, [username]);
+
+  const handleResize = useCallback(() => {
+    console.log(window.outerWidth)	
+    if (window.outerWidth <= 280) {	
+      setScreenSize('small');	
+    } else if (window.outerWidth <= 500) {	
+      setScreenSize('medium');	
+    } else if (window.outerWidth <= 888) {	
+        setScreenSize('middle');	
+    } else if (window.outerWidth<= 1500){	
+      setScreenSize('large');	
+    } else{
+      setScreenSize('xlarge');
+    }
+  }, []);
+
+  useEffect(() => {
+    handleResize();
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, [handleResize]);
+
+  function responsiveText(){
+    if (screenSize==='small') {
+      return 'h5'
+    }
+    if (screenSize==='medium') {
+      return 'h4'
+    }
+    if (screenSize==='middle'){
+      return 'h4'
+    }
+    if (screenSize==='large') {
+      return 'h3'
+    }
+    return 'h3'
+  }
+
+  function responsivePad(){
+    if (screenSize==='small') {
+      return '20px'
+    }
+    if (screenSize==='medium') {
+      return '20px'
+    }
+    if (screenSize==='middle'){
+      return '20px'
+    }
+    if (screenSize==='large') {
+      return '13px'
+    }
+    return '13px'
+  }
+
+  function responsiveButton(){
+    if (screenSize==='small') {
+      return {size:'157px', padding: '5px'}
+    }
+    if (screenSize==='medium') {
+      return {size:'157px', padding: '7px'}
+    }
+    if (screenSize==='middle') {
+      return {size:'157px', padding: '7px'}
+    }
+    if (screenSize==='large') {
+      return {size:'187px', padding: '10px'}
+    }
+    return {size:'187px', padding: '10px'}
+  }
+
 
   if (!loaded) {
     return <Loading />;
@@ -84,10 +155,12 @@ function StudentProfile() {
             sx={{
               display: 'flex',
               flexDirection: 'row',
+              justifyContent: 'space-between',
               padding: '2rem',
             }}
-          >
-            <Box sx={{ marginTop: '0.7rem', marginRight: '0.8rem' }}>
+          > 
+            <Box sx={{display: 'flex', flexDirection:'row'}}>
+            <Box sx={{ marginTop: '0.7rem', marginRight: '0.6rem' }}>
               <LetterAvatar
                 fontSize="40px"
                 width="60px"
@@ -96,37 +169,35 @@ function StudentProfile() {
               />
             </Box>
             <Typography
-              variant="h3"
+              variant={responsiveText()}
               sx={{
                 display: 'inline-flex',
                 fontWeight: 'bold',
                 padding: '13px',
+                paddingTop:responsivePad()
               }}
             >
               {`${studentDetails.fname} ${studentDetails.lname}`}
             </Typography>
+            </Box>
             <Box
               sx={{
                 display: 'flex',
                 flexDirection: 'column',
                 // margin: '20px 30px 20px 650px',
-                marginTop: '0.7rem',
-                marginLeft: '45rem',
+                alignItems: 'center',
+                marginTop: '20px',
               }}
             >
               {usertype === 'Student' && (
                 <MyButton
-                  width="187px"
-                  paddinghorizontal="10px"
-                  paddingvertical="10px"
+                  width={responsiveButton().size}
+                  paddinghorizontal={responsiveButton().padding}
+                  paddingvertical={responsiveButton().padding}
                   variant="contained"
-                  sx={{ mb: 2, justifyContent: '', variant: 'contained' }}
+                  sx={{ mb: 2, justifyContent: 'center', variant: 'contained' }}
                   onClick={() => navigate(editProfileRoute)}
                 >
-                  <Icon
-                    component={EditProfileIcon}
-                    sx={{ width: '50px', height: '32px' }}
-                  />
                   Edit Profile
                 </MyButton>
               )}
